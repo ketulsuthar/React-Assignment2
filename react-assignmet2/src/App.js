a8be82eb2fbbb00d44db39a1c48009e870730bb8
ketulkumar <k2lsuthar@gmail.com>
2020-07-15 22:56:35 -0400

    Added solutions

--- react-assignmet2/src/App.js
+++ react-assignmet2/src/App.js
@@ -1,9 +1,34 @@
 import React ,{Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Validation from './Validation/Validation';
+import Char from './Char/Char';
+
 class App extends Component{
+
+  state = {
+    userInput :''
+  }
+
+  textChange = (event)=> {
+    this.setState({userInput:event.target.value});
+  }
+
+  deleteCharHandler = ( index ) => {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({userInput: updatedText});
+  }
+  
   render() {
+
+    const charList = this.state.userInput.split('').map((ch, index) => {
+      return <Char 
+        character={ch} 
+        key={index}
+        clicked={() => this.deleteCharHandler(index)} />;
+    });
 
     return (
       <div className="App">
@@ -17,6 +42,15 @@ class App extends Component{
         </ol>
         <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
         <hr />
+
+        <input
+          type="test"
+          onChange={this.textChange}
+          value={this.state.userInput}></input>
+        <p>{this.state.userInput}</p>
+        <Validation
+          inputLength={this.state.userInput.length}/>
+        {charList}
       </div>
     );
   }
